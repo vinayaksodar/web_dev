4896f99ad22b92dfc358139533fdd857c3f2f7c3
vinayaksodar <vinayakmsodar@gmail.com>
2025-02-18 00:03:10 +0530

    update simple tutorial

--- javascript/gallery-start/main.js
+++ javascript/gallery-start/main.js
@@ -5,14 +5,38 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images =['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText ={
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Closeup of a human eye',
+    'pic3.jpg': 'Closeup of a human eye',
+    'pic4.jpg': 'Closeup of a human eye',
+    'pic5.jpg': 'Closeup of a human eye'}
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + image);
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);
+        
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    currentAppearance = btn.getAttribute('class');
+    if (currentAppearance === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {    
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor= 'rgba(255, 255, 255, 0)';
+
+    
+}
+});
+
